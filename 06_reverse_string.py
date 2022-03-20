106ffc2fbef0bc63eea7f4bf962191edab4bec21
Stiliyan St <65528270+stiliyan172@users.noreply.github.com>
2022-03-20 16:04:49 +0200

    Add files via upload

---
+++ 06_reverse_string.py
@@ -0,0 +1,11 @@
+def reverse_text(text):
+    i = len(text) - 1
+    while i >= 0:
+        yield text[i]
+        i -= 1
+
+
+for char in reverse_text("step"):
+    print(char, end='')
+
+print("".join(reverse_text("Python")))
